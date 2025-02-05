8695fb4b5acdd1356788bf3514008add3fec7f5b
Pablo Castillo <parracastilloj6@gmail.com>
2025-02-05 03:58:00 -0400

    chore: update environment variables and add .env to gitignore

--- src/routes/config.js
+++ src/routes/config.js
@@ -1,12 +1,6 @@
-export const PORT= process.env.PORT ||  5000;
-
-// src/config.js
-export const DB_USER = "postgres"
-export const DB_HOST = "localhost"
-export const DB_PASSWORD = "1234567890"
-export const DB_DATABASE = "hotel_chain"
-export const DB_PORT = 5432
-
-
-
-  
+export const PORT = process.env.PORT || 5000;
+export const DB_USER = process.env.DB_USER || "postgres";
+export const DB_HOST = process.env.DB_HOST || "localhost";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "1234567890";
+export const DB_DATABASE = process.env.DB_DATABASE || "hotel_chain";
+export const DB_PORT = process.env.DB_PORT || 5432;
