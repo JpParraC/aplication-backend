a44b779d8131d320fcd8ec2bd1c9a3a9105cf6b6
Pablo Castillo <parracastilloj6@gmail.com>
2025-02-05 11:14:22 -0400

    chore: update DATABASE_URL for local development and improve auth middleware comments

--- src/middleware/authmiddleware.js
+++ src/middleware/authmiddleware.js
@@ -1,35 +1,35 @@
-
 import jwt from 'jsonwebtoken';
-import { rolePermissions } from '../config/permissions.js'; // Asegúrate de ajustar la ruta
-
+import { rolePermissions } from '../config/permissions.js'; // Asegúrate de que la ruta sea correcta
 
 export const authenticateAndAuthorize = (requiredPermission) => {
   return (req, res, next) => {
     // Obtener el token del encabezado Authorization
     const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
+    const token = authHeader && authHeader.split(' ')[1];  // Obtener el token (Bearer <token>)
 
     if (!token) {
       return res.status(401).json({ message: 'Token no proporcionado' });
     }
 
-    //crificar token 
+    // Verificar el token
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
       if (err) {
         return res.status(403).json({ message: 'Token inválido o expirado' });
       }
 
-      
       req.user = user;
 
-      // Verificar si el usuario tiene el rol necesario y el permiso requerido
-      const userPermissions = rolePermissions[user.role_id] || []; // Obtenemos los permisos del rol
+      // Obtener los permisos del rol del usuario
+      const userPermissions = rolePermissions[user.role_id] || []; // Asignamos permisos basados en el rol del usuario
 
+      // Verificar si el usuario tiene el permiso requerido
       if (!userPermissions.includes(requiredPermission)) {
-        return res.status(403).json({ message: 'No tiene permisos para acceder a esta ruta' });
+        return res.status(403).json({
+          message: `Acceso denegado: Se requiere el permiso '${requiredPermission}' para esta acción`
+        });
       }
 
-      // Si el token es válido y el usuario tiene el permiso, continuamos con la ruta
+      // Si el usuario tiene el permiso, continuamos con la ruta
       next();
     });
   };
